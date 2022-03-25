26086019dc4abb2f83d9fd9d849cdb08c8682feb
Chriszao <chris.f.assis18@gmail.com>
2022-03-24 21:40:24 -0300

    feat: Add Order entity

---
+++ backend/src/modules/orders/typeorm/entities/Order.ts
@@ -0,0 +1,28 @@
+import {
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import User from '@modules/users/typeorm/entities/User';
+
+@Entity('orders')
+class Order {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Order;
